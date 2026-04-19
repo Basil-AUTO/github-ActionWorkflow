694ca3046e7d43047fef66cee56e001dd082bb48
Basil Elbalaawi <basilsaifyazan@gmail.com>
2026-04-19 19:27:23 +0300

    add new bash scritp

---
+++ src/test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# src/test.sh
+
+EXPECTED= "Hello, Test!"
+
+OUTPUT= $(node -e "console.log(require('./src/app')('test'))")
+
+if( "$ OUTPUT" == "$EXPORTED"); then
+
+echo " Test Passed "
+
+exit 0
+
+else
+
+echo "Test faild"
+
+exit 1
+
+fi
