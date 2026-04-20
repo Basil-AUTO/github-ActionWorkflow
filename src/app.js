d451faeda45ca519b07af607742cd922d921dab9
Basil Elbalaawi <basilsaifyazan@gmail.com>
2026-04-20 20:57:11 +0300

    add new comment ' final 88888 '

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ function greet(name) {
 
 module.exports = greet;
 
-if (require.main == module) {
-
+if (require.main === module) {
     console.log(greet("world"));
 }
--- src/test.sh
+++ src/test.sh
@@ -1,23 +1,12 @@
 #!/bin/bash
 
-#src/test.sh
-EXPECTED= "Hello, test!"
+EXPECTED="Hello, test!"
+OUTPUT=$(node -e 'console.log(require("./src/app")("test"))')
 
-OUTPUT= $(node -e "console.log(require(`./src/app`)(`test`))")
-
-if( "$OUTPUT" == "$EXPORTED"); then
-
-echo "Test Passed!"
-
-exit 0
-
+if [ "$OUTPUT" = "$EXPECTED" ]; then
+  echo "Test Passed!"
+  exit 0
 else
-
-echo "Test completely failed!"
-
-exit 1
-
+  echo "Test completely failed!"
+  exit 1
 fi
-
-# This script runs a simple test to check if the output of the app function matches the expected value. If it does, it prints "Test Passed!" and exits with a status of 0. If it doesn't, it prints "Test completely failed!" and exits with a status of 1.
-#1233
