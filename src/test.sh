154715f8853e598e714f7ade1ddeca204b5bba96
Basil Elbalaawi <basilsaifyazan@gmail.com>
2026-04-20 20:07:20 +0300

    add new comment 'change to realese/**'

--- src/test.sh
+++ src/test.sh
@@ -18,3 +18,5 @@ echo "Test completely failed!"
 exit 1
 
 fi
+
+# This script runs a simple test to check if the output of the app function matches the expected value. If it does, it prints "Test Passed!" and exits with a status of 0. If it doesn't, it prints "Test completely failed!" and exits with a status of 1.
